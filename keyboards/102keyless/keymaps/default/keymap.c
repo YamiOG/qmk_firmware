81061e43ce460681673a9a66029710f13ee06243
YamiOG <noahy1214@gmail.com>
2020-04-27 17:20:59 -0400

    Made First Macro

--- keyboards/102keyless/keymaps/default/keymap.c
+++ keyboards/102keyless/keymaps/default/keymap.c
@@ -1,9 +1,13 @@
 #include "102keyless.h"
 
+enum{
+	YT_SEARCH
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		KC_MAIL, KC_B, 
+		YT_SEARCH, KC_B, 
 		KC_C, KC_D, 
 		KC_E, KC_F)
 
@@ -25,39 +29,17 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch(keycode){
+		case YT_SEARCH:
+			if(!record->event.pressed){
+				register_code(KC_LGUI);
+				unregister_code(KC_LGUI);
+				SEND_STRING("https://youtu.be/Jsh7MSpuAi4");
+				register_code(KC_ENTER);
+				unregister_code(KC_ENTER);
+				return false;
+			}
+			break;
+	}
 	return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-	if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_COMPOSE)) {
-		
-	} else {
-		
-	}
-
-	if (usb_led & (1 << USB_LED_KANA)) {
-		
-	} else {
-		
-	}
-
 }
