6d1dd49c36a140033f2298e7c9b79805e05ee739
YamiOG <noahy1214@gmail.com>
2020-04-27 16:45:54 -0400

    Made Basic 102Keyless Keyboard

---
+++ keyboards/102keyless/102keyless.c
@@ -0,0 +1,1 @@
+#include "102keyless.h"
---
+++ keyboards/102keyless/102keyless.h
@@ -0,0 +1,16 @@
+#ifndef HUNDREDTWO_KEYLESS_H
+#define HUNDREDTWO_KEYLESS_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+	K00, K01, \
+	K10, K11, \
+	K20, K21  \
+) { \
+	{ K00,   K01 }, \
+	{ K10,   K11 }, \
+	{ K20,   K21 }  \
+}
+
+#endif
