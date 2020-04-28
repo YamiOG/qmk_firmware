b110a064c65c5e306f9110dbe8d7a0ff0abf9c52
YamiOG <noahy1214@gmail.com>
2020-04-27 22:13:47 -0400

    Added Finished Keymap

--- keyboards/102keyless/keymaps/default/keymap.c
+++ keyboards/102keyless/keymaps/default/keymap.c
@@ -1,18 +1,19 @@
 #include "102keyless.h"
 
 enum{
-	YT_SEARCH
+	ST_OBS, YT_SEARCH,
+	
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		YT_SEARCH, KC_B, 
-		KC_C, KC_D, 
-		KC_E, KC_F)
+		ST_OBS, KC_F24, 
+		KC_F23, YT_SEARCH, 
+		KC_F22, KC_F21)
 
 };
-
+ 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 	
 
@@ -30,6 +31,16 @@ void matrix_scan_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch(keycode){
+		case ST_OBS:
+			if(!record->event.pressed){
+				register_code(KC_LGUI);
+				unregister_code(KC_LGUI);
+				SEND_STRING("OBS");
+				register_code(KC_ENTER);
+				unregister_code(KC_ENTER);
+				return false;
+			}
+			break;
 		case YT_SEARCH:
 			if(!record->event.pressed){
 				register_code(KC_LGUI);
